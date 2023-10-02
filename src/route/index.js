57c9802b227beb9a8ad31d551b26bdb1ff481edc
AleksandrL0579 <a.lelsan@ukr.net>
2023-10-02 12:05:06 +0300

    calk

--- src/route/index.js
+++ src/route/index.js
@@ -1,3 +1,18 @@
+// // Підключаємо роутер до бек-енду
+// const express = require('express')
+// const router = express.Router()
+
+// // Підключіть файли роутів
+// const test = require('./test')
+// // Підключіть інші файли роутів, якщо є
+
+// // Об'єднайте файли роутів за потреби
+// router.use('/', test)
+// // Використовуйте інші файли роутів, якщо є
+
+// // Експортуємо глобальний роутер
+// module.exports = router
+
 // Підключаємо роутер до бек-енду
 const express = require('express')
 const router = express.Router()
@@ -6,9 +21,17 @@ const router = express.Router()
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
 
+// const test2 = require('./test2')
+
+const calc = require('./calc')
+
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
 // Використовуйте інші файли роутів, якщо є
+
+// router.use('/test', test2)
+
+router.use('/calc', calc)
 
 // Експортуємо глобальний роутер
 module.exports = router
